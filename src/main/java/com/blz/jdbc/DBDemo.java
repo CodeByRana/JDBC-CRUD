b960431ec50557b9c27f2c08e3e2f377c6b43697
deepakphpweb <dipakrana2760@gmail.com>
2022-01-09 12:21:02 +0530

    Added - Ability to create a payroll service database and have java program connect to database

--- src/main/java/com/blz/jdbc/DBDemo.java
+++ src/main/java/com/blz/jdbc/DBDemo.java
@@ -1,7 +1,27 @@
 package com.blz.jdbc;
 
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBDemo {
     public static void main(String[] args) {
         System.out.println("welcome to JDBC");
+
+        //step 1
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("Driver loaded");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Driver is not loaded");
+        }
+
+        //step 2
+        String JDBCURL = "jdbc:mysql://localhost:3307/payroll_services";
+        try {
+            DriverManager.getConnection(JDBCURL, "root", "pass");
+            System.out.println("Connection established");
+        } catch (SQLException e) {
+            System.out.println("Connection not established");
+        }
     }
 }
